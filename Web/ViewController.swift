b0a60845741f6ccc742b6ac35dc51248c2cfc1e7
doomout <winquake3@hanmail.net>
2022-12-21 17:21:38 +0900

    액션 함수, 아울렛 변수 추가

--- Web/ViewController.swift
+++ Web/ViewController.swift
@@ -1,19 +1,43 @@
-//
-//  ViewController.swift
-//  Web
-//
-//  Created by 현구김 on 2022/12/21.
-//
-
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var txtUrl: UITextField!
+    @IBOutlet var myWebView: WKWebView!
+    @IBOutlet var myActivityIndicator: UIActivityIndicatorView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func btnGotoUrl(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnGoSite1(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnGoSite2(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnLoadHtmlString(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnLoadHtmlFile(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnStop(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func btnReload(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func btnGoBack(_ sender: UIBarButtonItem) {
+    }
+        
+    @IBAction func btnGoForward(_ sender: UIBarButtonItem) {
+    }
 }
 
